d304c57f77b7350a5059333a64c19b0d5b85fd08
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-18 20:21:59 +0300

    Update Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -1,12 +1,24 @@
-#pragma warning disable IDE0005
 using Microsoft.EntityFrameworkCore;
-#pragma warning restore IDE0005
 using server.Data;
+using server.Middleware;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Car Rental API",
+        Version = "v1",
+        Description = "API for managing car rentals"
+    });
+});
 
 // Add DbContext
 builder.Services.AddDbContext<CarRentalContext>(options =>
@@ -16,10 +28,21 @@ builder.Services.AddDbContext<CarRentalContext>(options =>
         b => b.MigrationsAssembly("server")
     ));
 
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Car Rental API V1");
+        c.RoutePrefix = "swagger";
+    });
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -33,12 +56,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
